390d3bf67032bd5ecae57b2596f1a0e517182523
xFlaye <eyala@yorku.ca>
2021-12-07 14:38:28 -0500

    cleaned code. Added gridHelper

--- demo.js
+++ demo.js
@@ -106,21 +106,6 @@ class World {
               color2: { value: new THREE.Color(0x104A80)},
               ratio: {value: innerWidth / innerHeight}
             },
-            // vertexShader: `varying vec2 vUv;
-            //   void main(){
-            //     vUv = uv;
-            //     gl_Position = vec4(position, 1.);
-            //   }`,
-            //       fragmentShader: `varying vec2 vUv;
-            //     uniform vec3 color1;
-            //     uniform vec3 color2;
-            //     uniform float ratio;
-            //     void main(){
-            //         vec2 uv = (vUv - 0.5) * vec2(ratio, 1.);
-            //       gl_FragColor = vec4( mix( color1, color2, length(uv)), 1. );
-            //     }`,
-            //     wireframe: true
-            // });
             vertexShader: `
                 varying vec2 vUv;
 
